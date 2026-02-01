f02dc3d35754636dbe8a694501d5f0fe18a80bc5
zzeidan11 <zzeidan2002@gmail.com>
2026-02-01 15:44:09 -0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Will You Be My Valentine? 💌",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! Hehehehe ❤️"           // Secret hover message
+            secretAnswer: "I don't just like you, I love you! Hehehehe ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14, 2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,16 +42,16 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOAHHH, You're The Bestest Ever 🥰💝❤️",  // Shows when they go past 5000%
-        high: "Forever And Always! 🚀💝",              // Shows when they go past 1000%
-        normal: "My Agnelllll! 🥰"                           // Shows when they go past 100%
+        extreme: "WOAHHH, You're the Bestest Ever 🥰💝❤️",  // Shows when they go past 5000%
+        high: "Forever and Always! ❤️😚💝",              // Shows when they go past 1000%
+        normal: "My Agnelll! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "I'm the luckiest man in the world! I love you So Much My Milia ❤️💝💖💝💓",
-        message: "I'm saving a big warm hug and a huge kiss for you!",
-        emojis: "💍💖😚💝☺️❤️💕"  // These will bounce around
+        title: "I'm the luckiest man in the world! I love you forever and always ❤️💝💖💝💓",
+        message: "I'm saving a big hug and an even bigger kiss for you!",
+        emojis: "💍💖😚💝☺️❤️💕💓"  // These will bounce around
     },
 
     // Color scheme for the website
